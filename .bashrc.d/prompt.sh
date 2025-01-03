e44a340d04b6f8db907e8e2023b063c3bded0189
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-03 22:18:28 +0000

    Move __prompt_command.sh, __git_ps1.sh and ps1.sh to prompt.sh

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,3 +1,17 @@
+# Usage: ps1
+#
+# Toggles the PS1 prompt between the short and long versions.
+#
+ps1() {
+	case $PS1 in
+	'\$ ')
+		PS1="\[\e[32m\]\u@\h\[\e[m\]:\[\e[34m\]\w\[\e[33m\]\$(__git_ps1)\[\e[m\]\n\$ "
+		;;
+	*)
+		PS1='\$ '
+	esac
+}
+
 # Usage: PROMPT_COMMAND='__prompt_command'
 #
 # Sets terminal title - e.g. `jfox@fedora:~/.config/bash (master)`.
