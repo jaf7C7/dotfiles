77a80e86f5db443fa1276336934108d33136a742
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-01-24 02:31:25 +0000

    Update bash

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -26,18 +26,18 @@ __prompt_command() {
 	printf '\033]0;%s\007' "${PWD//~/\~}$(__git_ps1)" # & disown $!
 }
 
-# Usage: PS1='\w$(__git_ps1) \$ '
-#
-# A much simplified (and much quicker) version of the prompt
-# which ships with `git`.
-#
-__git_ps1() {
-	if ! git status -s >/dev/null 2>&1; then
-		return
-	fi
-	local current=$(git branch --show-current)
-	if test -z "$current"; then
-		current=$(git rev-parse --short HEAD)
-	fi
-	printf ' (%s)' "$current"
-}
+## Usage: PS1='\w$(__git_ps1) \$ '
+##
+## A much simplified (and much quicker) version of the prompt
+## which ships with `git`.
+##
+#__git_ps1() {
+#	if ! git status -s >/dev/null 2>&1; then
+#		return
+#	fi
+#	local current=$(git branch --show-current)
+#	if test -z "$current"; then
+#		current=$(git rev-parse --short HEAD)
+#	fi
+#	printf ' (%s)' "$current"
+#}
